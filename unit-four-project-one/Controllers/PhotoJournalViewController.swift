916be94480259f7b36b6bdd50bb63fbbedf5cdea
levi davis <levidavis@pursuit.org>
2019-09-27 12:51:14 -0400

    First commit

---
+++ unit-four-project-one/Controllers/PhotoJournalViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoJournalViewController.swift
+//  unit-four-project-one
+//
+//  Created by Levi Davis on 9/27/19.
+//  Copyright © 2019 Levi Davis. All rights reserved.
+//
+
+import UIKit
+
+class PhotoJournalViewController: UIViewController {
+
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ unit-four-project-oneTests/unit_four_project_oneTests.swift
@@ -0,0 +1,34 @@
+//
+//  unit_four_project_oneTests.swift
+//  unit-four-project-oneTests
+//
+//  Created by Levi Davis on 9/27/19.
+//  Copyright © 2019 Levi Davis. All rights reserved.
+//
+
+import XCTest
+@testable import unit_four_project_one
+
+class unit_four_project_oneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
